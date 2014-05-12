f18335c9cf5e5bd9d1c6e60d335dd4e387556e65
Sabadios <sabadios@gmail.com>
2014-05-12 13:34:18 -0400

    advance mongo support and fixes
    advance mongo support and fixes

--- Cherry.Platform/Modules/Cherry.Engine/src/main/java/org/Cherry/Modules/Mongo/Middleware/MongoRepositoryService.java
+++ Cherry.Platform/Modules/Cherry.Engine/src/main/java/org/Cherry/Modules/Mongo/Middleware/MongoRepositoryService.java
@@ -1,36 +1,38 @@
 /*******************************************************************************
  * Copyright (c) 2013-2014 Cherry Platform
- * 
+ *
  * The contents of this file are subject to the terms of one of the following
  * open source licenses: Apache 2.0 or LGPL 3.0 or LGPL 2.1 or CDDL 1.0 or EPL
  * 1.0 (the "Licenses"). You can select the license that you prefer but you may
  * not use this file except in compliance with one of these Licenses.
- * 
+ *
  * You can obtain a copy of the Apache 2.0 license at
  * http://www.opensource.org/licenses/apache-2.0
- * 
+ *
  * You can obtain a copy of the LGPL 3.0 license at
  * http://www.opensource.org/licenses/lgpl-3.0
- * 
+ *
  * You can obtain a copy of the LGPL 2.1 license at
  * http://www.opensource.org/licenses/lgpl-2.1
- * 
+ *
  * You can obtain a copy of the CDDL 1.0 license at
  * http://www.opensource.org/licenses/cddl1
- * 
+ *
  * You can obtain a copy of the EPL 1.0 license at
  * http://www.opensource.org/licenses/eclipse-1.0
- * 
+ *
  * See the Licenses for the specific language governing permissions and
  * limitations under the Licenses.
- * 
- * 
+ *
+ *
  * Cherry Platform is a registered trademark of Sabadios
- * 
+ *
  * Contributors:
  * Cristian Malinescu - initial design, API and implementation
  *******************************************************************************/
 package org.Cherry.Modules.Mongo.Middleware;
+
+import static org.Cherry.Utils.Utils.failIfEmpty;
 
 import java.net.UnknownHostException;
 import java.util.Set;
@@ -48,6 +50,11 @@ import com.mongodb.MongoServerSelectionException;
 
 @Singleton
 public class MongoRepositoryService extends ServiceTemplate {
+  public DB getDB(final String name) {
+    failIfEmpty(name, "Database name can't be empty!");
+    return getMongo().getDB(name);
+  }
+
   @PostConstruct
   void postConstruct() {
     try {
@@ -105,7 +112,7 @@ public class MongoRepositoryService extends ServiceTemplate {
   static public final String DEFAULT_DB_NAME = "test";
 
   /**
-   * 
+   *
    */
   private static final long serialVersionUID = 1L;
 }
